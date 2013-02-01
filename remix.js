13a110ce7f2d542dcf3416da6a5f8c0b02770f4b
tkell <thor.kell@mail.mcgill.ca>
2013-02-01 12:21:10 -0500

    And define the error handler properly...

--- remix.js
+++ remix.js
@@ -343,37 +343,6 @@ function createJRemixer(context, jquery, apiKey) {
  
     };
 
-
-
-    function fileErrorHandler(e) {
-      var msg = '';
-
-      switch (e.code) {
-        case FileError.QUOTA_EXCEEDED_ERR:
-          msg = 'QUOTA_EXCEEDED_ERR';
-          break;
-        case FileError.NOT_FOUND_ERR:
-          msg = 'NOT_FOUND_ERR';
-          break;
-        case FileError.SECURITY_ERR:
-          msg = 'SECURITY_ERR';
-          break;
-        case FileError.INVALID_MODIFICATION_ERR:
-          msg = 'INVALID_MODIFICATION_ERR';
-          break;
-        case FileError.INVALID_STATE_ERR:
-          msg = 'INVALID_STATE_ERR';
-          break;
-        default:
-          msg = 'Unknown Error';
-          break;
-      };
-
-      console.log('Error: ' + msg);
-    }
-
-
-
     function isQuantum(a) {
         return 'start' in a && 'duration' in a;
     }
@@ -391,6 +360,33 @@ function createJRemixer(context, jquery, apiKey) {
     return remixer;
 }
 
+
+function fileErrorHandler(e) {
+  var msg = '';
+
+  switch (e.code) {
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msg = 'QUOTA_EXCEEDED_ERR';
+      break;
+    case FileError.NOT_FOUND_ERR:
+      msg = 'NOT_FOUND_ERR';
+      break;
+    case FileError.SECURITY_ERR:
+      msg = 'SECURITY_ERR';
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msg = 'INVALID_MODIFICATION_ERR';
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msg = 'INVALID_STATE_ERR';
+      break;
+    default:
+      msg = 'Unknown Error';
+      break;
+  };
+
+  console.log('Error: ' + msg);
+}
 
 function euclidean_distance(v1, v2) {
     var sum = 0;
